459b26b8cc513bfdbccdd1e458aa91a64ddf963b
Andrew Paterson <andrewpaterson@Andrews-MacBook-Air.local>
2015-02-20 13:42:25 -0500

    Initial Commit

---
+++ BookCookTests/BookCookTests.swift
@@ -0,0 +1,36 @@
+//
+//  BookCookTests.swift
+//  BookCookTests
+//
+//  Created by Andrew Paterson on 2/20/15.
+//  Copyright (c) 2015 Andrew Paterson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BookCookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
